a4ae03089db6adf5e7f7ee24bb6440a14a5e7e7c
janarthanan.d <janarthanan.d@ppts-HP-EliteBook-840-G2>
2021-07-09 11:56:52 +0530

    Updated.

--- custom_product/static/js/website_sale_recently_viewed.js
+++ custom_product/static/js/website_sale_recently_viewed.js
@@ -16,6 +16,46 @@ $(document).ready(function ($) {
     $('.help-popover').popover({
     container: 'body'
   	})
+  	
+  	
+  	if (document.getElementById('product_info') != null) {
+  	
+  		$.ajax({
+			url: "/website-cart-change",
+			type: 'POST',
+			async : false,
+
+			data:{},
+			success: function(result){
+				console.log(result);
+				var result = JSON.parse(result);
+				console.log(result);
+				
+				if (parseInt(result['weight'], 0) < 2) {
+				
+					document.getElementById("product_info").style.display = "block";
+				
+					}
+				else{
+					
+					document.getElementById("product_info").style.display = "none";
+									
+				}
+				
+				if (document.getElementById("cart_weight")) {
+            		
+            		document.getElementById("cart_weight").innerHTML = result['gram'];
+					document.getElementById("cart_weight1").innerHTML = result['weight'];
+					document.getElementById("cart_lbs").innerHTML = result['lbs'];
+            		
+        			}
+				
+			},
+			}); 
+  	
+  		}
+  	
+  	
 });
 
 publicWidget.registry.WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
@@ -213,11 +253,16 @@ publicWidget.registry.WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
 				var result = JSON.parse(result);
 				console.log(result);
 				
-				if (parseInt(result['weight'], 0) < 1) {
+				if (parseInt(result['weight'], 0) < 2) {
 				
 					document.getElementById("product_info").style.display = "block";
 				
 					}
+				else{
+					
+					document.getElementById("product_info").style.display = "none";
+									
+				}
 				
 				if (document.getElementById("cart_weight")) {
             		
