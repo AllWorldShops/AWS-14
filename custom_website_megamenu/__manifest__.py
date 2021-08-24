e44ed23557f2dbc86d07e0af2438bff686b59827
info@allworldshops.com <info@allworldshops.com>
2021-08-24 13:37:54 +0530

    Added category mega menu

---
+++ custom_website_megamenu/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Custom Website Category Megamenu',
+    'summary': '',
+    'version': '14.0.1.0',
+    'category': 'Website',
+    'summary': """
+       This module will add websites templates.
+    """,
+    'description': """ This module will add websites Category templates which can be used for website designs. 
+    """,
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'website': 'https://www.pptssolutions.com',
+    'depends': [
+    'custom_brand_page',
+    ],
+    'data': [
+        'templates/category_templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'active': False,
+}
