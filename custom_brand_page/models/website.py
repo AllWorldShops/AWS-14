020e6bdcbe6acc4e7b3a1d4fc452727a12d1b44f
janarthanan.d <janarthanan.dhanasekar@pptservices.com>
2022-03-07 14:35:43 +0530

    Added website sale hide price app.

--- custom_brand_page/models/website.py
+++ custom_brand_page/models/website.py
@@ -9,6 +9,20 @@ class Website(models.Model):
 
     _inherit = "website"
     
+    
+    def get_parent_category(self):
+        """
+        Collect all the parent category. and return with category name and category ID
+        @Author : Angel Patel (24/09/2020)
+        :return: cat_array
+        """
+        category_obj = self.env['product.public.category'].sudo().search([('parent_id', '=', False),('website_id','in',[request.env['website'].sudo().get_current_website().id])])
+        cat_array = [{'name': "All",'id':""}]
+        if category_obj:
+            for cat in category_obj:
+                cat_array.append({'name': cat.name, 'id': cat.id})
+        return cat_array
+    
     def _force_website(self, website_id):
         res = super(Website, self)._force_website(website_id)
         if request:
